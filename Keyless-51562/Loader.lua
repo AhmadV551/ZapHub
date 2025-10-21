6be38ce41c16241612470a1c9f854649f28d77b4
AhmadV551 <selrach.zone@gmail.com>
2025-10-21 13:48:39 +0800

    Update Loader.lua

--- Keyless-51562/Loader.lua
+++ Keyless-51562/Loader.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/LKjHv2vG"))()
 
-loadstring(game:HttpGet("https://gist.githubusercontent.com/AhmadV551/d1506de76bd603a9e0bc7ad0404f152a/raw/61598231a80ec835dcdd82b6f65224f0536a63dd/Visual%2520for%2520Zaphub"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/AhmadV551/d1506de76bd603a9e0bc7ad0404f152a/raw/61598231a80ec835dcdd82b6f65224f0536a63dd/Visual%2520for%2520Zaphub", true))()
